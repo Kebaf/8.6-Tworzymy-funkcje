2eeb295a9a853cc46555b3580da5d27a464278bb
ENTER <ENTER>
2017-04-01 23:26:57 +0200

    return w nawiasach

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 var triangle1Area = getTriangleArea(10,15),
-triangle2Area = getTriangleArea(2,3),
+triangle2Area = getTriangleArea(0,3),
 triangle3Area = getTriangleArea(10,2);
 function getTriangleArea(a, h) {
 	if (a <= 0) {
 		console.log('Nieprawidłowe dane')
 	} else if (h <= 0) {
 		console.log('Nieprawidłowe dane')
-	}
+	} else {
 	return a * h / 2
+	}
 }
 console.log( getTriangleArea(10,6), triangle1Area, triangle2Area, triangle3Area);
