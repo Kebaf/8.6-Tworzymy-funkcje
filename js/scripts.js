7a843092e786b280ea4266ebf0a34da51c5ee45e
ENTER <ENTER>
2017-04-01 21:53:15 +0200

    zadanie 8.6

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var triangle1Area = getTriangleArea(10,15),
+triangle2Area = getTriangleArea(2,3),
+triangle3Area = getTriangleArea(10,2);
+function getTriangleArea(a, h) {
+	if (a <= 0) {
+		console.log('Nieprawidłowe dane')
+	} else if (h <= 0) {
+		console.log('Nieprawidłowe dane')
+	}
+	return (a * h / 2)
+}
+console.log( getTriangleArea(10,6), triangle1Area, triangle2Area, triangle3Area);
